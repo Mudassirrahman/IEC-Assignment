b627cba94b04861d80055eaf8a5a9db1c1031251
Mudassirrahman <93732269+Mudassirrahman@users.noreply.github.com>
2022-01-11 08:23:11 +0500

    -some bugs fixed

--- bakery-order-site-final/main.js
+++ bakery-order-site-final/main.js
@@ -49,25 +49,6 @@ window.onscroll =() =>{
 
   //   REVIEW SLIDE   //
 
-  /*var swiper = new Swiper(".review-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-  loop:true,
-      
-    breakpoints: {
-      0: {slidesperview: 1,
-    },
-    640: {slidesperview: 2, },
-    768: {slidesperview: 2,},
-    1024:{slidesperview: 3,},
-    },})  */
-
-
-
     var swiper = new Swiper(".review-slider", {
       spaceBetween: 20,
       centeredSlides: true,
@@ -91,3 +72,32 @@ window.onscroll =() =>{
         },
       },
     });
+
+
+    // js for navbar activeation
+
+    let section = document.querySelectorAll('section');
+      let navLinks = document.querySelectorAll('header .navbar a');
+      
+      window.onscroll = () => {
+      
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+      
+        section.forEach(sec => {
+      
+          let top = window.scrollY;
+          let height = sec.offsetHeight;
+          let offset = sec.offsetTop - 150;
+          let id = sec.getAttribute('id');
+      
+          if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header .navbar a[href*=' + id + ']').classList.add('active');
+            });
+          };
+      
+        });
+      
+      }
